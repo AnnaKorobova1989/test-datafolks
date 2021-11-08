f8478496429e0c3d938f7b402b4c79e8c4fe510b
Anna <korobovaanna89@gmail.com>
2021-11-08 05:58:42 +0300

    финал

--- source/js/script.js
+++ source/js/script.js
@@ -97,3 +97,24 @@ document.addEventListener('keydown', function(e) {
     modalClose(modalActive);
   }
 })
+
+function Maskjs () {
+  if (document.querySelector('input[type="tel"]')) {
+    var phoneFields = document.querySelectorAll('input[type="tel"]');
+    var phoneFieldMask = {
+      mask: '+7(000)000-00-00'
+    };
+
+    phoneFields.forEach(function (field) {
+      var mask = new window.IMask(field, phoneFieldMask);
+      mask.value = field.value;
+
+      field.addEventListener('focus', function () {
+        field.value = '+7(';
+        mask.updateValue();
+      });
+    });
+  }
+};
+
+Maskjs();
